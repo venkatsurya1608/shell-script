08c9108e9d4223e898adf5ab29cb94443321e94f
venkatsurya <venkatsurya1608@gmail.com>
2024-04-18 12:03:39 +0530

    some-message

---
+++ 05-variable.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+echo "Please enter username::"
+
+read -s USERNAME #here USERNAME is variable here -s is when you enter details they hide user and passward
+
+echo "Please enter password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
