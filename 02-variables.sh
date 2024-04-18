62b1a4a11e1ecfa550b054e04e0471b6ad32d6c2
venkatsurya <venkatsurya1608@gmail.com>
2024-04-18 11:26:02 +0530

    some-message

---
+++ 02-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Function for Ram's dialogue
+PERSON1=Ram
+PERSON2=venkat
+
+# Conversation between Ram and Venkat
+
+echo ::ram_dialogue "Hey Venkat, how's it going?"
+echo ::venkat_dialogue "Hi Ram, I'm doing well, thanks for asking. How about you?"
+echo ::ram_dialogue "I'm good too, just busy with work as usual."
+echo ::venkat_dialogue "Yeah, I know how that feels. Anything interesting happening?"
+echo ::ram_dialogue "Not much, just the usual routine. What about you?"
+echo ::venkat_dialogue "I've been working on a new project lately, it's quite challenging but exciting."
+echo ::ram_dialogue "That sounds interesting! Tell me more about it."
+echo ::venkat_dialogue "Sure, I'd love to. It's about..."
+
+# Add more dialogue as needed
+
+exit 0
