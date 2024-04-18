b1a2f0a8290e254fa739d7a568b9edae28a6c6ef
venkatsurya <venkatsurya1608@gmail.com>
2024-04-18 10:53:15 +0530

    some-message

--- 02-variables.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-echo "Ramesh:: Hello Suresh, How are you?"
-echo "Suresh:: Hi Ramesh, I am fine. How are you?"
-echo "Ramesh: I am fine too. how is your work?"
-echo "Suresh:: not bad. I am thinking to upgrade to DevOps"
