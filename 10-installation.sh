4dd098061574e15f5bc12f61e8ef3f9bec2993a4
venkatsurya <venkatsurya1608@gmail.com>
2024-04-19 14:24:30 +0530

    some-message

---
+++ 10-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+echo "please run this script wit root access"
+exit 1
+else 
+      echo "you are super user"
+
+
+dnf install mysql -y
