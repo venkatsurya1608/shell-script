2b0406a23b3b02def284f364952c32de9ad037ea
venkatsurya <venkatsurya1608@gmail.com>
2024-04-21 13:08:11 +0530

    some-message

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.com
 
 VALIDATE(){
    if [ $1 -ne 0 ]
