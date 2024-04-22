4806570cf049fe45ba45e6b14d8c860244b225ae
venkatsurya <venkatsurya1608@gmail.com>
2024-04-22 14:51:00 +0530

    some-message

--- 15-ownproject.sh
+++ 15-ownproject.sh
@@ -29,5 +29,15 @@ VALIDATE(){
       echo "you are super user"
     fi
 
-    dnf install mysql -y &>>$LOGFILE
-    VALIDATE $? "Installing MySQL"
+    for i in $@
+do
+    echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done
