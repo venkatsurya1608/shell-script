9fa31049ff37a67b70636fe59785e836db02ef94
venkatsurya <venkatsurya1608@gmail.com>
2024-04-23 13:36:30 +0530

    expense

--- 16-db-mysql.sh
+++ 16-db-mysql.sh
@@ -46,3 +46,6 @@ if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
+    else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
