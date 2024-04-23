bab8cb0506169792e6a423b67fbb9d8946ba5dc2
venkatsurya <venkatsurya1608@gmail.com>
2024-04-23 16:38:53 +0530

    expense

--- backend.sh
+++ backend.sh
@@ -61,6 +61,8 @@ VALIDATE $? "Extracting backend code"
 npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
-#check your repo and path
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
+
 
 
