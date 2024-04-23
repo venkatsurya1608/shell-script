779ddf29d800b86bfdb9cb938b64692b5a03b6a6
venkatsurya <venkatsurya1608@gmail.com>
2024-04-23 13:27:54 +0530

    expense

--- 16-db-mysql.sh
+++ 16-db-mysql.sh
@@ -39,3 +39,5 @@ VALIDATE $? "Starting Mysql server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 VALIDATE $? "Mysql root password setup"
+
+mysql -h db.venkatdevops1608.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
