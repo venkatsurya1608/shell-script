12baaa1dca11ffddddd76264ea0f8be468c3111d
venkatsurya <venkatsurya1608@gmail.com>
2024-04-26 14:57:50 +0530

    expense

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=4
 
 while IFS= read -r line 
 do 
    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1" )
-   FOLDER=$(echo $line | awk -F " " '{print $NF}')
+   $FOLDER=$(echo $line | awk -F " " '{print $NF}')
    if [ $USAGE -ge $DISK_THRESHOLD ]
    then 
-      MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE \n"
+      echo $FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
       fi 
       done <<<$DISK_USAGE 
