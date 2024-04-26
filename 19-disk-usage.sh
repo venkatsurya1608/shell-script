f6b8bc80f2dd45c7bb6a0b7396bb1f89d82f2a97
venkatsurya <venkatsurya1608@gmail.com>
2024-04-26 16:10:25 +0530

    expense

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
+MESSAGE=""
 
 while IFS= read -r line
 do
@@ -9,6 +10,11 @@ do
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"   #here + previous and present 6 kanna ekuva ga vastahe chupithudi /n means new line 
     fi
 done <<< $DISK_USAGE
+echo -e "Message: $MESSAGE"
+
+echo "$MESSAGE" | mail -s "Disk Usage Alert" ramanasurya3470@gmail.com   
+
+# echo "body" | mail -s "subject" to-address
