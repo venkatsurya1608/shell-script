cc17d949e713565fbec8dd111f4bf5ff73b1e6b0
venkatsurya <venkatsurya1608@gmail.com>
2024-04-26 14:54:01 +0530

    expense

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -3,10 +3,9 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=4
 
-
 while IFS= read -r line 
 do 
-   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "% -f1" )
+   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1" )
    FOLDER=$(echo $line | awk -F " " '{print $NF}')
    if [ $USAGE -ge $DISK_THRESHOLD ]
    then 
